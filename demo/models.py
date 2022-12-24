d5ad42a8f4773498eeffbdc7100fdd3d4e5356f0
leesure <xluow5965@gmail.com>
2022-12-25 01:42:03 +1100

    modify datetime fields datatype

--- demo/models.py
+++ demo/models.py
@@ -1,4 +1,3 @@
-import time
 
 from django.db import models
 from django.db.models import AutoField, CharField, IntegerField, DateTimeField
@@ -11,9 +10,9 @@ class DemoModel(models.Model):
     last_name=CharField(max_length=100)
     age=IntegerField(default=0)
     gender=CharField(default="male",null=False, max_length=20)
-    create_time = DateTimeField(default=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    create_time = DateTimeField(auto_now_add=True)
     create_user = CharField(max_length=100)
-    update_time = DateTimeField(default=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    update_time = DateTimeField(auto_now=True)
     update_user = CharField(max_length=100)
     is_deleted = IntegerField(default=0)
 
--- demo/tests.py
+++ demo/tests.py
@@ -1,4 +1,4 @@
-import time
+import time,datetime
 
 from django.test import TestCase
 
@@ -7,3 +7,5 @@ if __name__=="__main__":
 
     # 格式化成2016-03-20 11:45:39形式
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    # print(type(datetime.datetime.now()))
+    print(type(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
